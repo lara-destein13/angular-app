d23515013b83bfe3bc4aaf2a601f24e4d85b01e8
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-04-13 17:59:20 -0400

    add four components

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewsComponent } from './news/news.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TasksComponent,
+    NewsComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
