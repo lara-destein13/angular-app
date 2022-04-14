0c2b46f258cee06d26f5f513836eab34d7e08c4b
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-04-13 22:46:41 -0400

    style toolbar

---
+++ angular-app/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { HomeComponent } from "./home/home.component";
+import { NewsComponent } from "./news/news.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { ToolsComponent } from "./tools/tools.component";
