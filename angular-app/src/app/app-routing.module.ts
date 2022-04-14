9fba451bfa379f9b8fd84bdf6332027d9b309dc3
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-04-13 21:49:50 -0400

    get toutes working for each component

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from "./news/news.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { ToolsComponent } from "./tools/tools.component";
+
+const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: 'news-component', component: NewsComponent },
+  { path: 'tasks-component', component: TasksComponent },
+  { path: 'tools-component', component: ToolsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
